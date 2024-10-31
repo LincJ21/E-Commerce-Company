38435e5d460910b48f39ad4b936163f85a28ea24
Josué de Jesús Torres Torres <josutorres@utb.edu.co>
2024-10-31 16:33:40 -0500

    falta login

--- app.py
+++ app.py
@@ -2,13 +2,9 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from infrastructure.routers.suggestion_router import router as suggestion_router
-<<<<<<< Updated upstream
-from interface.auth.oauth_router import router as oauth_router
 from starlette.middleware.sessions import SessionMiddleware
-=======
+from infrastructure.routers.suggestion_router import router as suggestion_router
 from infrastructure.routers import cart_router, router_location, router_product_type, product_router
->>>>>>> Stashed changes
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -23,7 +19,6 @@ app.include_router(product_router.router)
 
 # Montar la carpeta de archivos estáticos (JavaScript y CSS)
 app.mount("/static", StaticFiles(directory="templates/static"), name="static")
-app.add_middleware(SessionMiddleware, secret_key="contraseña al por mayor")
 
 # Configurar la carpeta de plantillas
 templates = Jinja2Templates(directory="templates")
@@ -31,14 +26,20 @@ templates = Jinja2Templates(directory="templates")
 
 # Incluir routers
 app.include_router(suggestion_router)
-app.include_router(oauth_router)
-
 
 # Ruta principal para cargar el HTML en la raíz
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+@app.get("/formulario", response_class=HTMLResponse)
+async def read_root(request: Request):
     return templates.TemplateResponse("formulario.html", {"request": request})
+
+@app.get("/carrito", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("carrito_compra.html", {"request": request})
 
 # Iniciar el servidor
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
